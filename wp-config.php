e160361bcff1a8c293bb36c53e0e0cb5bd132305
ohhlanla <ohhlanla@gmail.com>
2018-03-26 15:40:14 +0700

    fix password for db

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'procar');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'j4**9c+p');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
